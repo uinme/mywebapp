8a6b63bab3a829ea5da4907d9bf668c1ad513b25
uinme <uinme.gm@gmail.com>
2020-09-07 14:11:39 +0900

    add functions to be used in UserController class

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -1,8 +1,10 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
 
 import model.UserModel;
 
@@ -22,9 +24,8 @@ public class UserDAO extends DAO {
         user = new UserModel();
         user.setId(result.getInt("id"));
         user.setUsername(result.getString("username"));
-        user.setEncrypedPassword(result.getString("encrypted_password"));
-        user.setCreatedAt(result.getDate("created_at"));
-        user.setUpdatedAt(result.getDate("updated_at"));
+        user.setCreatedAt(result.getTimestamp("created_at"));
+        user.setUpdatedAt(result.getTimestamp("updated_at"));
       }
 
       statement.close();
@@ -34,5 +35,54 @@ public class UserDAO extends DAO {
     }
     
     return user;
+  }
+
+  public UserModel findByEmail(String email) {
+    Connection connection = getConnection();
+
+    UserModel user = null;
+    try {
+      PreparedStatement statement = connection.prepareStatement(
+        "SELECT * FROM user WHERE email = ?"
+      );
+      statement.setString(1, email);
+      ResultSet result = statement.executeQuery();
+
+      while(result.next()) {
+        user = new UserModel();
+        user.setId(result.getInt("id"));
+        user.setUsername(result.getString("username"));
+        user.setCreatedAt(result.getTimestamp("created_at"));
+        user.setUpdatedAt(result.getTimestamp("updated_at"));
+      }
+
+      statement.close();
+      connection.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return user;
+  }
+
+  public int createUser(String email, String encryptedPassword, Timestamp createdAt, Timestamp updatedAt) {
+    Connection connection = getConnection();
+
+    int line = 0;
+    try {
+      PreparedStatement statement = connection.prepareStatement(
+        "INSERT INTO user VALUES (NULL, NULL, ?, ?, ?, ?)"
+      );
+      statement.setString(1, email);
+      statement.setString(2, encryptedPassword);
+      statement.setTimestamp(3, createdAt);
+      statement.setTimestamp(4, updatedAt);
+      statement.executeUpdate();
+      line = statement.executeUpdate();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return line;
   }
 }
