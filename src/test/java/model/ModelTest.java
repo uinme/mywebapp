d70edc4fc47330079fa7077e83f19180bcd1d354
uinme <uinme.gm@gmail.com>
2020-09-09 21:20:40 +0900

    add test for getFieldNames() in Model class

---
+++ src/test/java/model/ModelTest.java
@@ -0,0 +1,37 @@
+package model;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Test;
+
+public class ModelTest {
+  @Test
+  public void getFieldNames() {
+    UserModel user = new UserModel();
+    List<String> fieldNames = user.getFieldNames();
+
+    Iterator<String> expectedFieldNames = Arrays.asList(
+      "serialVersionUID",
+      "id",
+      "username",
+      "email",
+      "encryptedPassword",
+      "createdAt",
+      "updatedAt"
+    ).iterator();
+
+    for (String fieldName : fieldNames) {
+      expectedFieldNames.hasNext();
+
+      Boolean expected = true;
+      Boolean actual = fieldName.contains(expectedFieldNames.next());
+
+      assertThat(actual, is(expected));
+    }
+  }
+}
