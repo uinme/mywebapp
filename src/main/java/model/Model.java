c61b0d37032306242decb26c02a402ca1c396fa1
uinme <uinme.gm@gmail.com>
2020-09-09 23:08:55 +0900

    modify getFieldName method to get only field gived SQL annotation

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -5,11 +5,16 @@ import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+
+import annotation.SQLColumn;
 
 public class Model implements Serializable {
   private static final long serialVersionUID = 1L;
@@ -25,11 +30,10 @@ public class Model implements Serializable {
   public List<String> getFieldNames() {
     Class<Model> clazz = (Class<Model>) this.getClass();
     Field fields[] =  clazz.getDeclaredFields();
-    List<String> fileldNames = new ArrayList<>();
-
-    for (int i = 0; i < fields.length; i++) {
-      fileldNames.add(fields[i].getName());
-    }
+    List<String> fileldNames = Arrays.stream(fields)
+      .filter(f -> f.isAnnotationPresent(SQLColumn.class))
+      .map(f -> f.getName())
+      .collect(Collectors.toList());
 
     return fileldNames;
   }
