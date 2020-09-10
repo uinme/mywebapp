a0cd91d7248a679ad81c119c7ef9dc6aa03e7cb6
uinme <uinme.gm@gmail.com>
2020-09-10 13:23:53 +0900

    delete some functions

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -1,20 +1,12 @@
 package model;
 
 import java.io.Serializable;
-import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-
-import annotation.SQLColumn;
 
 public class Model implements Serializable {
   private static final long serialVersionUID = 1L;
@@ -25,16 +17,5 @@ public class Model implements Serializable {
     Connection connection = dataSource.getConnection();
     
     return connection;
-  }
-
-  public List<String> getFieldNames() {
-    Class<Model> clazz = (Class<Model>) this.getClass();
-    Field fields[] =  clazz.getDeclaredFields();
-    List<String> fileldNames = Arrays.stream(fields)
-      .filter(f -> f.isAnnotationPresent(SQLColumn.class))
-      .map(f -> f.getName())
-      .collect(Collectors.toList());
-
-    return fileldNames;
   }
 }
