44bbc4a46017b754549f1d58638b12545a3bdc63
uinme <uinme.gm@gmail.com>
2020-09-10 13:27:30 +0900

    remove test regarding SQLCoulumn annotation

--- src/test/java/model/ModelTest.java
+++ src/test/java/model/ModelTest.java
@@ -3,34 +3,14 @@ package model;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+import org.hamcrest.core.IsNull;
 import org.junit.Test;
 
 public class ModelTest {
-  @Test
-  public void getFieldNames() {
-    UserModel user = new UserModel();
-    List<String> fieldNames = user.getFieldNames();
-
-    Iterator<String> expectedFieldNames = Arrays.asList(
-      "id",
-      "username",
-      "email",
-      "encryptedPassword",
-      "createdAt",
-      "updatedAt"
-    ).iterator();
-
-    for (String fieldName : fieldNames) {
-      expectedFieldNames.hasNext();
-
-      Boolean expected = true;
-      Boolean actual = fieldName.contains(expectedFieldNames.next());
-
-      assertThat(actual, is(expected));
-    }
-  }
+  
 }
