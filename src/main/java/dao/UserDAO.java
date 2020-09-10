6f23c220335e3423fe9a8d7a02c25ed3cbf5fa09
uinme <uinme.gm@gmail.com>
2020-09-10 15:18:33 +0900

    add updateUser funciton

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -5,7 +5,12 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.google.common.base.CaseFormat;
+
+import model.Model;
 import model.UserModel;
 
 public class UserDAO extends DAO {
@@ -82,6 +87,28 @@ public class UserDAO extends DAO {
       statement.setTimestamp(3, createdAt);
       statement.setTimestamp(4, updatedAt);
       statement.executeUpdate();
+      line = statement.executeUpdate();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return line;
+  }
+
+  public int updateUser(UserModel user) {
+    Connection connection = getConnection();
+
+    int line = 0;
+    try {
+      PreparedStatement statement = connection.prepareStatement(
+        "UPDATE user SET username = ?, email = ?, encrypted_password = ?, created_at = ?, updated_at = ? WHERE id = ?"
+      );
+      statement.setString(1, user.getUsername());
+      statement.setString(2, user.getEmail());
+      statement.setString(3, user.getEncryptedPassword());
+      statement.setTimestamp(4, user.getCreatedAt());
+      statement.setTimestamp(5, user.getUpdatedAt());
+      statement.setInt(6, user.getId());
       line = statement.executeUpdate();
     } catch (Exception e) {
       e.printStackTrace();
