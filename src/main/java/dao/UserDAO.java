f07f2eb4ec4bfbf3d9ea61debd6e369ef615b4c9
uinme <uinme.gm@gmail.com>
2020-09-11 11:46:06 +0900

    add searching funtions

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -44,6 +44,66 @@ public class UserDAO extends DAO {
     return user;
   }
 
+  public UserModel findById(int id) {
+    Connection connection = getConnection();
+
+    UserModel user = null;
+    try {
+      PreparedStatement statement = connection.prepareStatement(
+        "SELECT * FROM user WHERE id = ?"
+      );
+      statement.setInt(1, id);
+      ResultSet r = statement.executeQuery();
+
+      while (r.next()) {
+        user = new UserModel();
+        user.setId(r.getInt("id"));
+        user.setUsername(r.getString("username"));
+        user.setEmail(r.getString("email"));
+        user.setEncrypedPassword(r.getString("encrypted_password"));
+        user.setCreatedAt(r.getTimestamp("created_at"));
+        user.setUpdatedAt(r.getTimestamp("updated_at"));
+      }
+
+      statement.close();
+      connection.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return user;
+  }
+
+  public UserModel findByUsername(String username) {
+    Connection connection = getConnection();
+
+    UserModel user = null;
+    try {
+      PreparedStatement statement = connection.prepareStatement(
+        "SELECT * FROM user WHERE username = ?"
+      );
+      statement.setString(1, username);
+      ResultSet result = statement.executeQuery();
+
+      while(result.next()) {
+        user = new UserModel();
+        user.setId(result.getInt("id"));
+        user.setUsername(result.getString("username"));
+        user.setEmail(result.getString("email"));
+        user.setEncrypedPassword(result.getString("encrypted_password"));
+        user.setCreatedAt(result.getTimestamp("created_at"));
+        user.setUpdatedAt(result.getTimestamp("updated_at"));
+      }
+
+      statement.close();
+      connection.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return user;
+  }
+
   public UserModel findByEmail(String email) {
     Connection connection = getConnection();
 
@@ -74,18 +134,19 @@ public class UserDAO extends DAO {
     return user;
   }
 
-  public int createUser(String email, String encryptedPassword, Timestamp createdAt, Timestamp updatedAt) {
+  public int createUser(String username, String email, String encryptedPassword, Timestamp createdAt, Timestamp updatedAt) {
     Connection connection = getConnection();
 
     int line = 0;
     try {
       PreparedStatement statement = connection.prepareStatement(
-        "INSERT INTO user VALUES (NULL, NULL, ?, ?, ?, ?)"
+        "INSERT INTO user VALUES (NULL, ?, ?, ?, ?, ?)"
       );
-      statement.setString(1, email);
-      statement.setString(2, encryptedPassword);
-      statement.setTimestamp(3, createdAt);
-      statement.setTimestamp(4, updatedAt);
+      statement.setString(1, username);
+      statement.setString(2, email);
+      statement.setString(3, encryptedPassword);
+      statement.setTimestamp(4, createdAt);
+      statement.setTimestamp(5, updatedAt);
       statement.executeUpdate();
       line = statement.executeUpdate();
 
